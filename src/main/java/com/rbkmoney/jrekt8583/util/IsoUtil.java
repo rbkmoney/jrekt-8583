e6da33e4244d0b7e07901a329dca99ff8f722032
Pavel Popov <popoffpavel@yandex.ru>
2019-02-05 20:34:57 +0300

    Bump j8583 lib; Add iso field dict; Some fixes

--- src/main/java/com/rbkmoney/jrekt8583/util/IsoUtil.java
+++ src/main/java/com/rbkmoney/jrekt8583/util/IsoUtil.java
@@ -2,6 +2,9 @@ package com.rbkmoney.jrekt8583.util;
 
 import com.rbkmoney.jrekt8583.IsoField;
 import com.solab.iso8583.IsoMessage;
+import com.solab.iso8583.IsoValue;
+
+import java.util.function.Consumer;
 
 public class IsoUtil {
 
@@ -11,6 +14,21 @@ public class IsoUtil {
                         isoMessage.getObjectValue(isoField.getId()).toString(),
                         isoField.getLength()
                 );
+    }
+
+    public static void setIfExist(IsoMessage isoMessage, IsoField isoField, Consumer<String> consumer) {
+        int numericCode = isoField.getId();
+        if (isoMessage.hasField(numericCode)) {
+            consumer.accept(getStringFieldValue(isoMessage, isoField));
+        }
+    }
+
+    public static <T> IsoValue<T> buildIsoValue(IsoField isoField, T value) {
+        return buildIsoValue(isoField, value, isoField.getLength());
+    }
+
+    public static <T> IsoValue<T> buildIsoValue(IsoField isoField, T value, int length) {
+        return isoField.getIsoType().value(value, length);
     }
 
 }
